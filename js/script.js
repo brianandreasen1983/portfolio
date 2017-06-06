e0a6eeb13d5d6e31736bb4d82d0a564323ead082
Brian Andreasen <bspinnfade@gmail.com>
2017-06-06 16:47:52 -0500

    New changes
    Changing layoutg completely for a more professional look for ease of
    reading and navigation.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-(function(){
+$(document).ready(function () {
+    //alert("ready for DOMination!");
     
+    //Click event handlers
+    /*Intention is to hide only the professional background section when clicked.*/
     
+    /*$('#background').click(function(){
+        $('.thumbnail').toggle();
+    });
     
+
+    $('#webdevproj').click(function(){
+           $('.thumbnail').toggle();
+    });
     
+ 
+    $('#csharp').click(function(){
+           $('.thumbnail').toggle();
+    });*/
     
-    
-})()
+    //$('h4').addClass('panel-title');
+});
