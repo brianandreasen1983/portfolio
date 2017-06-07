f716f1586d0f766393e3314afd787ab218dc1ec8
Brian Andreasen <bspinnfade@gmail.com>
2017-06-06 20:58:06 -0500

    dg
    ddfgdfg

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     //alert("ready for DOMination!");
-    
+
+    $('h4').addClass("panel-title");
+    $('ul').addClass("list-group");
+
+
+
     //Click event handlers
     /*Intention is to hide only the professional background section when clicked.*/
-    
+
     /*$('#background').click(function(){
         $('.thumbnail').toggle();
     });
@@ -17,9 +22,7 @@ $(document).ready(function () {
     $('#csharp').click(function(){
            $('.thumbnail').toggle();
     });*/
-    
-    $('h4').addClass("panel-title");
-    $('ul').addClass("list-group");
-    
-    
+
+
+
 });
