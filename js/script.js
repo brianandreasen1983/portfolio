4cc8148165b1cc448e18eb2d09f9e5256af23dcb
Brian Andreasen <bspinnfade@gmail.com>
2017-06-24 01:16:55 -0500

    dfg
    dfg

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     //alert("ready for DOMination!");
-
-    $('h4').addClass("panel-title");
-
-    $('#webdevproj').children().first().siblings().hide();
-    $('#csharp').children().first().siblings().hide();
     
-    //If you click on the Web Projects header it will toggle the projects to show and hide.
+    //Still need to clean up the code quite a bit.
+    
+    //Setting the h4 css class for bootstrap this mainly affects the green headers within the page.
+    var greenHeaders = $('h4').addClass("panel-title");
+    
+    //Hiding the web dev projects and c sharp projects on page load.
+    var hideWebProjects = $('#webdevproj').children().first().siblings().hide();
+    var hideCSharpProjects = $('#csharp').children().first().siblings().hide();
+    
+    //This block contains click handlers for the toggle functionality of background, web projects and c sharp project headers.
     var webDevProjects = $('#webdevproj').children().first().click(function () {
         $(webDevProjects).siblings().toggle();
     });
@@ -18,37 +22,36 @@ $(document).ready(function () {
     var background = $('#background').children().first().click(function(){
         $(background).siblings().toggle();
     });
-    //var background = $('#background').children().next().toggle();
+    
+    //Animations
 
-    function animateProfilePicture(profilePicture) {
-        var profilePicture = $('#profilepic');
-        profilePicture.hide();
-        profilePicture.fadeIn(2000);
-    };
+    //Functional component that provides the fade in for the profile picture.
+    var profilePicture = $('#profilepic');
+    profilePicture.hide();
+    profilePicture.fadeIn(2000);
 
-    function animateJumbotron(jumbotron) {
-        var jumbotron = $('.jumbotron');
-        jumbotron.hide();
-        jumbotron.animate().slideDown(1000);
-    };
+    //Functional component that provides the slide down animation of the jumbotron on load.
+    var jumbotron = $('.jumbotron');
+    jumbotron.hide();
+    jumbotron.animate().slideDown(1000);
 
-    function animateBackground(background) {
-        var background = $('#background');
-        background.hide();
-        background.animate().slideDown(2000);
-    };
+    //Functional component that provides the slide down animation to the background on load.
+    var background = $('#background');
+    background.hide();
+    background.animate().slideDown(2000);
 
-    function animateWebProjects(webProjects) {
-        var webProjects = $('#webdevproj');
-        webProjects.hide();
-        webProjects.slideDown(1500);
-    };
+    //Functional component that provides the slide down funtionality for the web projects on load.
+    var webProjects = $('#webdevproj');
+    webProjects.hide();
+    webProjects.slideDown(1500);
+    
 
-    function animateCsharpProjects(CsharpProjects) {
-        var CsharpProjects = $('#csharp');
-        CsharpProjects.hide();
-        CsharpProjects.slideDown(1500);
-    };
+    //Functional Component that provides the slide down functionality for the c sharp projects on load.
+    var CsharpProjects = $('#csharp');
+    CsharpProjects.hide();
+    CsharpProjects.slideDown(1500);
+
+    
 
     //    function animateNavigationBar(navBar) {
     //        var navBar = $('nav');
@@ -69,9 +72,5 @@ $(document).ready(function () {
     //    }
 
     //    animateNavigationBar();
-    animateJumbotron();
-    animateBackground();
-    animateProfilePicture();
-    animateWebProjects();
-    animateCsharpProjects();
+
 });
