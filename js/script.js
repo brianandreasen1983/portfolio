cd0a640b0ca9365dbb8d03717b46c763f03fc301
Brian Andreasen <bspinnfade@gmail.com>
2017-06-23 22:46:05 -0500

    Animations
    Adding animations to add some more pizazz to the portfolio.

--- js/script.js
+++ js/script.js
@@ -2,25 +2,41 @@ $(document).ready(function () {
     //alert("ready for DOMination!");
 
     $('h4').addClass("panel-title");
-
-
-    //Click event handlers
-    /*Intention is to hide only the professional background section when clicked.*/
-
-    /*$('#background').click(function(){
-        $('.thumbnail').toggle();
-    });
     
+    function showProfilePicture(profilePicture) {
+        var profilePicture = $('#profilepic');
+        profilePicture.hide();
+        profilePicture.fadeIn(2000);
+    };
 
-    $('#webdevproj').click(function(){
-           $('.thumbnail').toggle();
-    });
-    
- 
-    $('#csharp').click(function(){
-           $('.thumbnail').toggle();
-    });*/
+    function slideDownJumbotron(jumbotron) {
+        var jumbotron = $('.jumbotron');
+        jumbotron.hide();
+        jumbotron.animate().slideDown(2500);
+    };
 
+    function animateBackground(background) {
+        var background = $('#background');
+        background.hide();
+        background.animate().slideDown(2000);
+    };
 
+    function showWebProjects(webProjects) {
+        var webProjects = $('#webdevproj');
+        webProjects.hide();
+        webProjects.slideDown(2000);
+    };
+
+    function showCsharpProjects(CsharpProjects) {
+        var CsharpProjects = $('#csharp');
+        CsharpProjects.hide();
+        CsharpProjects.slideDown(2000);
+    };
+
+    slideDownJumbotron();
+    animateBackground();
+    showProfilePicture();
+    showWebProjects();
+    showCsharpProjects();
 
 });
