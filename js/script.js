9c37a1a10e5e14af612d25159f4c49b4c29339ad
Brian Andreasen <bspinnfade@gmail.com>
2017-06-25 23:16:01 -0500

    Animations
    New animations pushing to the website. Next steps are to implement SASS
    for CSS and stylize even further.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
 $(document).ready(function () {
     //alert("ready for DOMination!");
     
-    //Still need to clean up the code quite a bit.
-    
     //Setting the h4 css class for bootstrap this mainly affects the green headers within the page.
     var greenHeaders = $('h4').addClass("panel-title");
     
     //Hiding the web dev projects and c sharp projects on page load.
     var hideWebProjects = $('#webdevproj').children().first().siblings().hide();
     var hideCSharpProjects = $('#csharp').children().first().siblings().hide();
-    
-    //This block contains click handlers for the toggle functionality of background, web projects and c sharp project headers.
-    var webDevProjects = $('#webdevproj').children().first().click(function () {
-        $(webDevProjects).siblings().toggle();
-    });
 
-    var cSharpProjects = $('#csharp').children().first().click(function(){
-        $(cSharpProjects).siblings().toggle();
-    });
-
-    
-//    var background = $('#background').children().first().click(function(){
-//        background.siblings().toggle();
-//    });
-    
     //Animations
-
+    var webDevProjects = $('#webdevproj').children().first().click(function () {
+        webDevProjects.siblings().slideToggle(1000);
+    });
+    
+    var cSharpProjects = $('#csharp').children().first().click(function(){
+        cSharpProjects.siblings().slideToggle(1000);
+    });
+    
     //Functional component that provides the fade in for the profile picture.
     var profilePicture = $('#profilepic');
     profilePicture.hide();
@@ -47,10 +38,10 @@ $(document).ready(function () {
     webProjects.slideDown(1500);
     
 
-    //Functional Component that provides the slide down functionality for the c sharp projects on load.
-    var CsharpProjects = $('#csharp');
-    CsharpProjects.hide();
-    CsharpProjects.slideDown(1500);
+//    CSharpProjects.siblings().first().hide(); //targets first row
+//    CSharpProjects.siblings().first().next().hide(); //targets second row
+//    CSharpProjects.siblings().last().hide(); //targets third row
+
 
     
 
